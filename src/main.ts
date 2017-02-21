7c64e65e4f5e7ca6ac63ff8f9de975f06781d59c
rharris2825 <john.harris297@gmail.com>
2017-02-20 20:15:24 -0500

    Updated build process. Removed console.log call.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { App } from './app/app';
 
-console.log(process.env.PORT);
-
 const port = process.env.PORT as number;
 const env = process.env.NODE_ENV as string;
 const app = new App().appInstance;
